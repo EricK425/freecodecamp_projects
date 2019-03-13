f9a56057000c39efbea80af972e1401c14ab522b
Eric Kleihege <aggies425@gmail.com>
2019-03-13 13:10:32 -0500

    finish text/info on site

---
+++ responsive_web_design_projects/tribute_page/jscript.js
@@ -0,0 +1,7 @@
+// let vHeight = $(window).height();
+// let vWidth = $(window).width();
+// let imgDiv = $('#main');
+// console.log(vHeight);
+// console.log(vWidth);
+//
+// imgDiv.css({"height":vHeight,"width":vWidth});
